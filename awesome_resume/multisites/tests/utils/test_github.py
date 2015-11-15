35af1c3199e53ec8188d8bfff8296be408e73191
dobestan <dobestan@gmail.com>
2015-11-15 19:05:57 +0900

    update GithubRepository class : add README.md related functions

--- awesome_resume/multisites/tests/utils/test_github.py
+++ awesome_resume/multisites/tests/utils/test_github.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+import requests
+
 from multisites.utils.github import GithubRepository
 
 
@@ -10,6 +12,7 @@ class GithubUtilsTestCase(TestCase):
         self.github_repository = GithubRepository(self.username)
 
         self.valid_github_repository_url = "https://github.com/dobestan/awesome-dobestan"
+        self.valid_raw_readme_url = "https://raw.githubusercontent.com/dobestan/awesome-dobestan/master/README.md"
 
         self.invalid_username = 'invalid_dobestan'
         self.invalid_github_repository = GithubRepository(self.invalid_username)
@@ -26,4 +29,30 @@ class GithubUtilsTestCase(TestCase):
         )
         self.assertFalse(
             self.invalid_github_repository.is_repository_valid(),
+        )
+
+    def test_get_raw_readme_url(self):
+        raw_readme_url = self.github_repository.get_raw_readme_url()
+
+        self.assertEqual(
+            raw_readme_url,
+            self.valid_raw_readme_url,
+        )
+
+        response = requests.get(raw_readme_url)
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+    def test_get_readme_content(self):
+        readme_content = self.github_repository.get_readme_content()
+
+        self.assertIsNotNone(
+            readme_content,
+        )
+
+        self.assertEqual(
+            type(readme_content),
+            str,
         )
--- awesome_resume/multisites/utils/github.py
+++ awesome_resume/multisites/utils/github.py
@@ -2,10 +2,16 @@ import requests
 
 
 class GithubRepository(object):
+    """Github Repository related functions."""
+
+    # TODO: refactor this class with Github Developer API.
+    # https://developer.github.com/v3/
 
     def __init__(self, username, *args, **kwargs):
         self.username = username
+
         self.repository_url = self.get_repository_url()
+        self.raw_readme_url = self.get_raw_readme_url()
 
     def get_repository_url(self):
         """Return full github url with <username>/awesome-<username>."""
@@ -23,3 +29,20 @@ class GithubRepository(object):
         if response.status_code == 200:
             return True
         return False
+
+    def get_raw_readme_url(self, branch='master'):
+        """Return raw README.md url."""
+
+        raw_readme_url = "https://raw.githubusercontent.com/{username}/awesome-{username}/master/README.md".format(
+            username=self.username,
+        )
+        return raw_readme_url
+
+    def get_readme_content(self):
+        """Return README.md contents from repository."""
+
+        response = requests.get(self.raw_readme_url)
+
+        # default type of response.content is "bytes".
+        # return type should be a "str" with UTF8 encoding.
+        return response.content.decode('utf8')
